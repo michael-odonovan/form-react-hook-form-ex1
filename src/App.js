008f27443b87a98b9609d90ac38bd39e9455d20b
michael o'donovan <mikeodonovan3@gmail.com>
2022-06-18 17:46:54 +0100

    feat: turning example into a name form

--- src/App.js
+++ src/App.js
@@ -14,16 +14,22 @@ function App() {
 
       <h2>First React Hook Form</h2>
 
+        <label for="firstName">First Name:</label>
         {/* register your input into the hook by invoking the "register" function */}
-        <input defaultValue="test" {...register("example")} />
+        <input placeholder="Enter your first name..." {...register("firstName")} />
         
+        <label for="secondName">Second Name:</label>
         {/* include validation with required or other standard HTML validation rules */}
-        <input {...register("exampleRequired", { required: true })} />
+        <input placeholder="Enter your second name" {...register("secondName", { required: true })} />
 
         {/* create custom errors that will be returned when field validation fails  */}
         {errors.exampleRequired && <span>This field is required</span>}
         
-        <input class="submitButton" type="submit" />
+        <input class="submitButton" type="submit" value="Submit Form" />
+
+        <h2>My Name is:</h2>
+        <div id="firstNameOutput"></div>
+        <div id="secondNameOutput"></div>
 
       </form>
     </div>
