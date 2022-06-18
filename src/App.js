10aaece3a09f0c91dd25fc1c960ad399aef861e7
michael o'donovan <mikeodonovan3@gmail.com>
2022-06-18 17:33:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("example"));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+      <h2>First React Hook Form</h2>
+
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register("example")} />
+        
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("exampleRequired", { required: true })} />
+
+        {/* create custom errors that will be returned when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+        
+        <input class="submitButton" type="submit" />
+
+      </form>
     </div>
   );
 }
